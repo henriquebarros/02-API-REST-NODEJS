3c9a6e54503bef9da3fa26d30f106de196ff859f
henriquebarros <henriquebarros@live.com>
2024-01-29 20:48:48 -0300

    Testes automatizados - Categorizando os testes

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,24 +1,27 @@
-import { test, beforeAll, afterAll } from 'vitest'
+import { it, beforeAll, afterAll, describe } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
 
-beforeAll(async () => {
-  await app.ready()
-})
+describe('Transctions routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
 
-afterAll(async () => {
-  await app.close()
-})
+  afterAll(async () => {
+    await app.close()
+  })
 
-test('user can create a new transaction', async () => {
-  await request(app.server)
-    .post('/transactions')
-    .send({
-      title: 'New transction',
-      amount: 5000,
-      type: 'credit',
-    })
-    .expect(201)
+  // deve ser possível criar uma nova transação
+  it('shuold be able to create a new transaction', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transction',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201)
+  })
 })
 
 /**
